0d8a281a646f7b927a5f245993833f28ba93b6eb
Yuval Saraf <yuvalsaraf@gmail.com>
2015-06-27 18:01:46 -0700

    Completion of Task 1.

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,14 +1,58 @@
 'use strict';
 
 angular.module('myApp.directives', [])
-    .directive('comments', [function(){
+    .directive('comments', ['Comments', 'commentsMaxLevel', function(Comments, commentsMaxLevel) {
         return {
             restrict: 'E',
-            replace: true,
-            templateUrl : 'templates/directives/comments.html',
-            scope : {},
-            link : function(scope, element){
-                //TODO: Add your code
+            templateUrl: 'templates/directives/comments.html',
+            scope: {
+                list: '=?',
+                level: '=?'
+            },
+            controller: ['$attrs', function($attrs) {
+                this.commentsMaxLevel = Number($attrs.commentsMaxLevel || commentsMaxLevel);
+            }],
+            link: function(scope, element, attrs) {
+                if (attrs.list === undefined || attrs.level === undefined) {
+                    Comments.getList('777AAA').then(function(list) {
+                        scope.list = list;
+                        console.log(list);
+                    });
+                    scope.level = 1;
+                }
             }
         }
-    }]);
+    }])
+    .directive('comment', ['Comments', '$compile', function(Comments, $compile) {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/comment.html',
+            scope: {
+                comment: '=?',
+                level: '=?'
+            },
+            require: '^comments',
+            link: function(scope, element, attrs, commentsCtrl) {
+                var commentsExists = false;
+                scope.$watchCollection('comment.comments', function(newVal) {
+                    var elementChanged = false;
+                    if (!commentsExists && Array.isArray(newVal) && newVal.length > 0 && scope.level < commentsCtrl.commentsMaxLevel) {
+                        commentsExists = true;
+                        var elementStr = '<comments list="comment.comments" level="level + 1" comments-max-level="' + commentsCtrl.commentsMaxLevel + '"></comments>';
+                        $compile(elementStr)(scope, function(newElement){
+                            element.children().append(newElement);
+                        });
+                    } else if (commentsExists && (!Array.isArray(newVal) || newVal.length === 0 || scope.level >= commentsCtrl.commentsMaxLevel)) {
+                        commentsExists = false;
+                        element.find('>div>comments').remove();
+                    }
+                });
+                scope.reply = function() {
+                    scope.comment.comments.push(new Comments({
+                        txt: 'Text text text text'
+                    }));
+                }
+            }
+        }
+    }])
+    .constant('commentsMaxLevel', 5);
--- public/js/filters.js
+++ public/js/filters.js
@@ -1,9 +1,30 @@
 'use strict';
 
 angular.module('myApp.filters', [])
-    .filter('time', ['$filter', function($filter){
-        return function(input){
-            //TODO: Replace this code
-            return $filter('date')(input);
+    .filter('time', ['$filter', function() {
+        var msPerMinute = 60 * 1000;
+        var msPerHour = msPerMinute * 60;
+        var msPerDay = msPerHour * 24;
+        var msPerMonth = msPerDay * 30;
+        var msPerYear = msPerDay * 365;
+
+        return function(input) {
+            var msDate = Date.parse(input);
+            var date = new Date(msDate);
+            var elapsed = Date.now() - msDate;
+
+            if (elapsed < msPerMinute) {
+                return 'A few seconds ago';
+            } else if (elapsed < msPerHour) {
+                return Math.round(elapsed / msPerMinute) + ' minute' + (elapsed < msPerMinute * 2 ? '' : 's') + ' ago';
+            } else if (elapsed < msPerDay) {
+                return Math.round(elapsed / msPerHour) + ' hour' + (elapsed < msPerHour * 2 ? '' : 's') + ' ago';
+            } else if (elapsed < msPerMonth) {
+                return Math.round(elapsed / msPerDay) + ' day' + (elapsed < msPerDay * 2 ? '' : 's') + ' ago';
+            } else if (elapsed < msPerYear) {
+                return Math.round(elapsed / msPerMonth) + ' month' + (elapsed < msPerMonth * 2 ? '' : 's') + ' ago';
+            } else {
+                return moment(msDate).format('[On] DD MMMM YYYY [at] HH:mm:ss');
+            }
         };
     }]);
